d2ee15c94ff43a618b69a7c61469533b49140c3a
EpicGuy4000 <enjakovic@gmail.com>
2024-08-01 13:56:32 +0200

    Kata "Reduction Operations to Make the Array Elements Equal" from DesignGurus

---
+++ Katas.Net.Tests/EqualizerTests.cs
@@ -0,0 +1,12 @@
+namespace Katas.Net.Tests;
+
+public class EqualizerTests
+{
+    [TestCase(new[] {3, 5, 5, 2}, 5)]
+    [TestCase(new[] {11, 9, 7, 5, 3}, 10)]
+    [TestCase(new[] {8, 8, 8, 8}, 0)]
+    public void Sort(int[] nums, int expectedOutput)
+    {
+        Assert.That(Equalizer.Equalize(nums), Is.EqualTo(expectedOutput));
+    }
+}
---
+++ Katas.Net/Equalizer.cs
@@ -0,0 +1,31 @@
+namespace Katas.Net;
+
+public static class Equalizer
+{
+    public static int Equalize(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+        
+        ISortingAlgorithm sortingAlgorithm = new MergeSort();
+
+        var sortedNums = sortingAlgorithm.Sort(nums);
+        var minElement = sortedNums[0];
+        var index = sortedNums.Length - 1;
+        var lastElement = sortedNums[^1];
+        var actionCounter = 0;
+
+        while (sortedNums[index] != minElement)
+        {
+            if (sortedNums[index] != lastElement)
+            {
+                actionCounter += sortedNums.Length - index - 1;
+                lastElement = sortedNums[index];
+            }
+
+            actionCounter++;
+            index--;
+        }
+
+        return actionCounter;
+    }
+}
