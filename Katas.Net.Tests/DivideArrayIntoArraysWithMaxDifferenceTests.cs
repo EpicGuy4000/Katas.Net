8ed1f421693db3d7a45c54c2b7736670608cb943
EpicGuy4000 <enjakovic@gmail.com>
2024-08-01 14:39:17 +0200

    Kata "Divide Array Into Arrays With Max Difference" from DesignGurus

---
+++ Katas.Net.Tests/DivideArrayIntoArraysWithMaxDifferenceTests.cs
@@ -0,0 +1,17 @@
+namespace Katas.Net.Tests;
+
+public class DivideArrayIntoArraysWithMaxDifferenceTests
+{
+    [TestCase(new[] { 2, 6, 4, 9, 3, 7, 3, 4, 1 }, 3, new[] { 1, 2, 3 }, new[] { 3, 4, 4 }, new[] { 6, 7, 9 })]
+    [TestCase(new[] { 10, 12, 15, 20, 25, 30 }, 10, new[] { 10, 12, 15 }, new[] { 20, 25, 30 })]
+    [TestCase(new[] { 1, 2, 4, 5, 9, 10 }, 1)]
+    public void Sort(int[] nums, int k, params int[][] expectedOutput)
+    {
+        var output = DivideArrayIntoArraysWithMaxDifference.Divide(nums, k);
+        Assert.That(output, Has.Length.EqualTo(expectedOutput.Length));
+        for (var i = 0; i < output.Length; i++)
+        {
+            CollectionAssert.AreEqual(expectedOutput[i], output[i]);
+        }
+    }
+}
---
+++ Katas.Net/DivideArrayIntoArraysWithMaxDifference.cs
@@ -0,0 +1,24 @@
+namespace Katas.Net;
+
+public static class DivideArrayIntoArraysWithMaxDifference
+{
+    public static int[][] Divide(int[] nums, int k)
+    {
+        if (nums.Length % 3 != 0) return [];
+        
+        ISortingAlgorithm sortingAlgorithm = new MergeSort();
+
+        var sortedArray = sortingAlgorithm.Sort(nums);
+
+        var dividedArrays = new int[nums.Length / 3][];
+
+        for (var i = 0; i < sortedArray.Length - 2; i += 3)
+        {
+            if (sortedArray[i + 2] - sortedArray[i] > k) return [];
+
+            dividedArrays[i / 3] = sortedArray[i..(i + 3)];
+        }
+
+        return dividedArrays;
+    }
+}
